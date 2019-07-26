bd9042c36b7ab6660b7dc8aba44a2dbfd169adce
miha53cevic <44951515+miha53cevic@users.noreply.github.com>
2019-07-26 15:53:05 +0200

    added font size option

---
+++ ImageToASCII CSharp/ASCIIWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+using System.Drawing;
+
+namespace ImageToASCII_CSharp
+{
+    /// <summary>
+    /// Interaction logic for ASCIIWindow.xaml
+    /// </summary>
+    public partial class ASCIIWindow : Window
+    {
+        public ASCIIWindow()
+        {
+            InitializeComponent();
+        }
+
+        public void DrawASCIIArt(string art, Bitmap img, int Grouping, int FontSize)
+        {
+            // Setup text area size
+            textArea.Width  = img.Width * FontSize / Grouping * 0.75;
+            textArea.Height = img.Height * FontSize / Grouping;
+
+            textArea.FontSize = FontSize;
+            textArea.Text = art;
+        }
+    }
+}
